1599c4d71467ae25cd4aaa4147368d1d7c1dfd93
Angelo aroni <angeduff@gmail.com>
2015-05-21 21:27:47 +0200

    Añadimos dormulario validacines basica y valida email con BD

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+$(document).ready(function() {
+    $('#frmalta').validate({
+        rules: {
+            nombre: {
+                required: true
+                
+            },
+            apellido:{
+            	required: true
+            },
+            telefono:{
+            	required: true,
+            	digits : true,
+            	minlength : 9
+            },
+            email: {
+                email: true,
+                required: true,
+                remote: 'http://www.futbolistas.com/php/validar_email_db.php'
+            },
+            email2: {
+                equalTo: '#email'
+            },
+            conocido:{
+            	required: true
+            }
+
+
+        } //,
+        //para que se muestre los errores encima del formulario
+        //'wrapper': 'p',
+        //'errorLabelContainer':'#errores'        
+
+    });
+});
