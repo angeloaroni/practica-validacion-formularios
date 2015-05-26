9c72fe9f95d0f70d9d5089d0f3bdf5711ed49393
Angelo aroni <angeduff@gmail.com>
2015-05-26 21:24:14 +0200

    autocompletar nombre

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,8 +17,7 @@ $(document).ready(function() {
             email: {
                 email: true,
                 required: true,
-                remote: 'http://www.aaroni.infenlaces.com/php/validar_email_db.php'
-                    //remote: 'php/validar_email_db.php'
+                remote: 'http://www.futbolistas.com/php/validar_email_db.php'
             },
             email2: {
                 equalTo: '#email'
@@ -31,12 +30,12 @@ $(document).ready(function() {
                 digits: true,
                 minlength: 4,
                 maxlength: 5
-            },  
+            },
             pass: {
                 pass: true,
                 required: true
             },
-            pass2:{
+            pass2: {
                 equalTo: '#pass'
             }
 
@@ -50,21 +49,44 @@ $(document).ready(function() {
     //si el codigo postal son 4 digitos autocompleta con un 0 a la izquierda
     $('#codpostal').focusout(function() {
         var caracteres = $('#codpostal').val();
-        if (caracteres.length === 4){
+        if (caracteres.length === 4) {
             $('#codpostal').val('0' + caracteres);
         }
     });
     //verifica la seguridad de la passsword con plugin complexify
-    $('#complexify #pass').complexify({}, function (valid, complexity) {
+    $('#complexify #pass').complexify({}, function(valid, complexity) {
         var progressBar = $('#complexify #complexity-bar');
 
         progressBar.toggleClass('progress-bar-success', valid);
         progressBar.toggleClass('progress-bar-danger', !valid);
-        progressBar.css({'width': complexity + '%'});
+        progressBar.css({
+            'width': complexity + '%'
+        });
 
         $('#complexify #complexity').text(Math.round(complexity) + '%');
     });
-    
-    $('#pass').passField({showWarn: false, showTip: false, allowEmpty: false});
-    
+
+    $('#pass').passField({
+        showWarn: false,
+        showTip: false,
+        allowEmpty: false
+    });
+    //Para quese complete la provincia escribiendo el codigo postal
+    $('#codpostal').change(function() {
+        if ($('#pais option:selected').val() === 'ES/0/0') {
+            if ($(this).val() !== '') {
+                var dato = $(this).val();
+                if (dato.length >= 2) {
+                    dato = dato.substring(0, 2);
+                }
+                $('#provincia').val(dato);
+            }
+        }
+    });
+
+    $('#nombre,#apellido').on('change',function () {
+        var value = $(this).val();
+        $('#nomempresa').val(value);
+    });
+
 });
